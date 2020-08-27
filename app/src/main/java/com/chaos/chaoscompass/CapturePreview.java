055754692501243c489f4098d65f7731623bd667
faizanalibutt <fayzanalibutt@gmail.com>
2020-08-27 09:59:48 +0500

    camera preview added.

---
+++ app/src/main/java/com/chaos/chaoscompass/CapturePreview.java
@@ -0,0 +1,71 @@
+package com.chaos.chaoscompass;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CapturePreview extends SurfaceView implements SurfaceHolder.Callback{
+
+    public static Bitmap mBitmap;
+    SurfaceHolder holder;
+    static Camera mCamera;
+
+    public CapturePreview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,int height) {
+
+        if(mCamera != null){
+            Camera.Parameters parameters = mCamera.getParameters();
+            if(parameters != null){
+                parameters.getSupportedPreviewSizes();
+                mCamera.setParameters(parameters);
+                mCamera.setDisplayOrientation(90);
+                mCamera.startPreview();
+            }
+        }
+
+
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+            try {
+                mCamera = Camera.open();
+                mCamera.setPreviewDisplay(holder);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if(mCamera != null){
+            mCamera.stopPreview();
+            mCamera.release();
+        }
+    }
+
+//    public static void takeAPicture(){
+//
+//        Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+//            @Override
+//            public void onPictureTaken(byte[] data, Camera camera) {
+//
+//                BitmapFactory.Options options = new BitmapFactory.Options();
+//                mBitmap = BitmapFactory.decodeByteArray(data, 0, data.length, options);
+//            }
+//        };
+//        mCamera.takePicture(null, null, mPictureCallback);
+//    }
+}
